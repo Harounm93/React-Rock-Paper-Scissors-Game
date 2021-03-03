e258042e1d84034ec0daef145c750f63b29fde77
harounm93 <mahdiharoun93@gmail.com>
2021-03-03 14:49:49 +0000

    game logic fixed and score is updating correctly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,115 +1,100 @@
 import './App.css';
-import Hand from '../selections/index'
-import { useState, useEffect } from "react"
-import Input from '../Input/index'
+import Hand from '../selections/index';
+import { useState, useEffect } from 'react';
+import Input from '../Input/index';
 //import { Typography } from '@material-ui/core';
 
 function App() {
-  const [userScore,setUserScore] = useState(0)
-  const [ComputerScore,setComputerScore] = useState(0)
-  const [player, setPlayer] = useState("")
-  const [possibleMoves,setPossibleMoves] = useState(["rock","paper", "scissors"])
-  const [userMove, setUserMove] = useState("move")
+  const [userScore, setUserScore] = useState(0);
+  const [ComputerScore, setComputerScore] = useState(0);
+  const [player, setPlayer] = useState('');
+  const [possibleMoves, setPossibleMoves] = useState([
+    'rock',
+    'paper',
+    'scissors',
+  ]);
+  const [userMove, setUserMove] = useState('move');
+  const [count, setCount] = useState();
 
- 
-  let randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-  //randomize computer moves
-  function addPlayer(item){
-    setPlayer(item)
+  function handleMove(userMove) {
+    let randomMove =
+      possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    console.log(randomMove);
+    //set user move
+    setUserMove(userMove);
+
+    //Game logic is below
+
+    //draw scenario: return early
+
+    if (userMove === randomMove) {
+      console.log('its a draw');
+      return;
+    }
+
+    //win scenarios below
+    if (
+      (userMove === 'paper' && randomMove === 'rock') ||
+      (userMove === 'rock' && randomMove === 'scissors') ||
+      (userMove === 'scissors' && randomMove === 'paper')
+    ) {
+      console.log('you gained a point');
+      // addScore();
+      setUserScore(userScore + 1);
+      
+    } 
+    //lose scenarios below
+    else if (
+      (userMove === 'paper' && randomMove === 'scissors') ||
+      (userMove === 'scissors' && randomMove === 'rock') ||
+      (userMove === 'rock' && randomMove === 'paper')
+    ) {
+      console.log('you lost this round');
+      // minusScore();
+      
+      setComputerScore(ComputerScore + 1)
+    }
   }
-// adding user to game
-  
-  function changeMove(value){
-setUserMove(value)
+  //randomize computer moves from array
+  function addPlayer(item) {
+    setPlayer(item);
   }
-/// lock in player move when button is clicked
+  // input user name to game
 
- 
+  function changeMove(value) {
+    setUserMove(value);
+  }
+  /// set player move as state when button is clicked
 
- 
+  //     //functions to update scores
+  function addScore() {
+    return setUserScore(userScore + 1);
+  }
+
+  function minusScore() {
+    return setUserScore(userScore - 1);
+  }
+
   useEffect(() => {
-    
+    console.log('useffect is running');
 
-    //functions to update scores
- function addScore(){
-  return setUserScore(userScore + 1)
-        }
-  
-        function minusScore(){
-          return setUserScore(userScore + 1)
-                }
-//Game logic is below
+    console.log(`you picked ${userMove}`);
+  }, [userMove]);
 
-//paper choice
-      if (userMove === "paper" && randomMove === "rock" ){
-        console.log("you gained a point")
-        
-      }
-
-      if (userMove === "paper" && randomMove === "scissors" ){
-        console.log("you lost this round")
-        
-      }
-      if (userMove === "paper" && randomMove === "paper" ){
-        console.log("its a draw")
-        
-      }
-
-     
-
-// rock choice
-
-if (userMove === "rock" && randomMove === "rock" ){
-  console.log("its a draw")
-  
-
-}
-
-else if (userMove === "rock" && randomMove === "scissors" ){
-  console.log("you won this round")
- addScore()
-  
-}
-else if (userMove === "rock" && randomMove === "paper" ){
-  console.log("you lost round")
-  minusScore()
-  
-}
-
-// scissor choices
-
-if (userMove === "scissors" && randomMove === "paper" ){
-  console.log("you gained a point")
-  
-}
-
-if (userMove === "scissors" && randomMove === "rock" ){
-  console.log("you lost this round")
-  
-}
-if (userMove === "scissors" && randomMove === "scissors" ){
-  console.log("its a draw")
-  
-}
-
-      console.log("useffect is running")
-      console.log(`computer picked ${randomMove}`)
-      console.log( `you picked ${userMove}`)
-      
-  }, [randomMove, userScore, userMove], );
-
-
+  console.log('render');
   return (
-    <div className="App">
-    <div className = "scores">
-    <h1 className = "user"> {player} Score: {userScore}</h1>
-    <h1 className = "computer"> Computer Score: {ComputerScore}</h1>
-    </div>
-    <Input addPlayer = {addPlayer}/>
-      <header className="App-header">
-        <Hand userMove = {changeMove}/>
+    <div className='App'>
+      <div className='scores'>
+        <h1 className='user'>
+          {' '}
+          {player} Score: {userScore}
+        </h1>
+        <h1 className='computer'> Computer Score: {ComputerScore}</h1>
+      </div>
+      <Input addPlayer={addPlayer} />
+      <header className='App-header'>
+        <Hand userMove={changeMove} handleMove={handleMove} />
       </header>
-      
     </div>
   );
 }
--- src/components/selections/index.js
+++ src/components/selections/index.js
@@ -1,16 +1,18 @@
 import './selections.css'
 
 
-function Hand ({userMove}){
+function Hand ({handleMove}){
 console.log("User has chosen a move")
+
+
 
 // Player move is decided 
 
     return ( <div className = "container">
 
-<button className = "rock" onClick = {() => {userMove("rock")}} > 👊</button>
-    <button className = "paper" onClick = {() => {userMove("paper")}}> ✋</button>
-    <button className = "scissors" onClick = {() => {userMove("scissors")}}> ✌️ </button>
+<button className = "rock" onClick = {() => {handleMove("rock")}} > 👊</button>
+    <button className = "paper" onClick = {() => {handleMove("paper")}}> ✋</button>
+    <button className = "scissors" onClick = {() => {handleMove("scissors")}}> ✌️ </button>
     
 
     </div>)
